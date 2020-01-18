bd8da44f3e67228f58d2adee8d89bb6dbdaaffec
kazuya <rockun009@gmail.com>
2020-01-19 01:39:38 +0900

    Add typealias for R class

---
+++ corecomponent/androidcomponent/src/main/java/io/github/droidkaigi/confsched2020/rtypealias/AndroidRAttr.kt
@@ -0,0 +1,3 @@
+package io.github.droidkaigi.confsched2020.rtypealias
+
+typealias AndroidRAttr = android.R.attr
---
+++ corecomponent/androidcomponent/src/main/java/io/github/droidkaigi/confsched2020/rtypealias/AndroidRInteger.kt
@@ -0,0 +1,3 @@
+package io.github.droidkaigi.confsched2020.rtypealias
+
+typealias AndroidRInteger = android.R.integer
